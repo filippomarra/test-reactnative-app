760380fb5473d4c8d6bcb9829642b6c4a7bdcecd
Filippo Marra <42439691+filippomarra@users.noreply.github.com>
2023-05-10 22:25:02 +0200

    Update index.ts

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,3 +1,3 @@
 export const API_ENDPOINT =
     'https://www.pivotaltracker.com/services/v5/projects/2116794/stories';
-export const APP_TITLE = 'PAGOPA TEST';
+export const APP_TITLE = 'REACTNATIVE TEST';
